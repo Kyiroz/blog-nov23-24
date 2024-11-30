e3053d9b915c7ff660e858c89733481725614247
Kyiro <josereyesiriarte@gmail.com>
2024-11-30 11:37:55 -0400

    base page

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import PostListView, PostDetailView
 
 urlpatterns = [                             
-    path('', PostListView.as_view(), name='blog-home'), #Esto es el post-list
+    path('', PostListView.as_view(), name='home'), #Esto es el post-list
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'), #Esto es el post-detail
 ]
