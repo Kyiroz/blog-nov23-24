d9666729e84a9fa0da3c3f9d17314f356f99a4e7
Kyiro <josereyesiriarte@gmail.com>
2024-11-30 11:05:32 -0400

    detail

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Post(models.Model):   #Post es una tabla en la base de datos
@@ -10,3 +11,6 @@ class Post(models.Model):   #Post es una tabla en la base de datos
  
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self): #Cuando invoque al post detail, quiero que devuelva el id
+        return reverse('post-detail', kwars={'pk': self.pk})
